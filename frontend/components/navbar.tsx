5b9833c02449b1fcd5ce49d797df29c7513d5751
Alessandro Canossa <alessandro.canossa.99@gmail.com>
2022-04-01 19:41:03 +0200

    Added account button with icon

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -6,6 +6,9 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import FormControl from "react-bootstrap/FormControl";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
 const MyNavbar = () => {
   return (
     <Navbar bg="dark" variant="dark">
@@ -22,18 +25,24 @@ const MyNavbar = () => {
           </Link>
         </Nav>
 
-        <Form className="d-flex" method="get" action="">
+        <Form className="d-flex me-5" method="get" action="">
           {/* TODO: set action to get the search */}
           <FormControl
             type="search"
             placeholder="Search"
             className="me-2 bg-dark"
           />
-          <Button type="submit" variant="outline-success">
+          <Button type="submit" variant="outline-success" className="me-2">
             Search
           </Button>
         </Form>
 
+        <Link href="/account">
+          <Button variant="outline-light">
+            <FontAwesomeIcon icon={faUser} className="me-2"></FontAwesomeIcon>
+            Username
+          </Button>
+        </Link>
       </Container>
     </Navbar>
   );
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,8 @@
 // import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
