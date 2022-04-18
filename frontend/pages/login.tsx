df05ed1dd57d72b312327f0de972605825e283af
Alessandro Canossa <alessandro.canossa.99@gmail.com>
2022-04-18 22:57:00 +0200

    created login form

---
+++ frontend/pages/login.tsx
@@ -0,0 +1,52 @@
+import Head from "next/head";
+import Layout from "../components/layout";
+import FormInput from "../components/formInput";
+import LoginSocial from "../components/loginSocial";
+
+const Login = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>Log In</title>
+      </Head>
+
+      <div className="container flex flex-wrap mt-5 mx-auto p-5 bg-white max-w-xl ">
+        <div className=" w-full items-center mb-4">
+          <h2 className="text-2xl">Log into your account</h2>
+        </div>
+        <div className=" w-full mb-4 ">
+          <form action="/" method="GET">
+            <div>
+              <FormInput
+                inputType={"text"}
+                inputName={"username"}
+                inputLabel={"Username"}
+                errorMsg={""}
+              />
+              <FormInput
+                inputType={"password"}
+                inputName={"password"}
+                inputLabel={"Password"}
+                errorMsg={""}
+                // errorMsg={passwordError}
+                // changeHandler={handleChangePassword}
+              />
+            </div>
+            <label>
+              <input type="checkbox" name="rememberMe" className="mr-2"/>
+              Remember Me
+            </label>
+            <div className="text-center">
+              <button type={"submit"} className="btn btn_dark">
+                Log in
+              </button>
+            </div>
+          </form>
+        </div>
+        <LoginSocial />
+      </div>
+    </Layout>
+  );
+};
+
+export default Login;
