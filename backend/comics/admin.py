f6b84667ac043fd1702bcd34e7c1da2443fa1a9a
Alessandro Canossa <alessandro.canossa.99@gmail.com>
2022-05-09 14:18:49 +0200

    added genres and comics models

--- backend/comics/admin.py
+++ backend/comics/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Comic, Genre
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Genre)
+admin.site.register(Comic)
--- backend/comics/models.py
+++ backend/comics/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth import models as auth_models
 
 from . import managers
+
+
 # Create your models here.
 
 
@@ -16,3 +18,36 @@ class User(auth_models.AbstractUser):
     def __str__(self):
         return self.username
 
+
+class Genre(models.Model):
+    name = models.CharField(unique=True, max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Comic(models.Model):
+    ONGOING = 'O'
+    HIATUS = 'H'
+    COMPLETED = 'C'
+    CANCELLED = 'D'
+    STATUS = [
+        (ONGOING, 'ONGOING'),
+        (HIATUS, 'HIATUS'),
+        (COMPLETED, 'COMPLETED'),
+        (CANCELLED, 'CANCELLED'),
+    ]
+
+    title = models.CharField(unique=True, max_length=255)
+    creator_id = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    genres = models.ManyToManyField(to=Genre)
+    watches = models.PositiveBigIntegerField(default=0)
+    follows = models.PositiveBigIntegerField(default=0)
+    rating = models.FloatField(default=0)
+    status = models.CharField(choices=STATUS, default=ONGOING, max_length=1)
+    summary = models.TextField()
+    thumbnail = models.ImageField(upload_to=f'static')
+    cover = models.ImageField(upload_to='static')
+
+    def __str__(self):
+        return self.title
