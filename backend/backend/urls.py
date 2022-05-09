eab52d75dae11b5f67d57bb205ddf41709d0f3e5
Alessandro Canossa <alessandro.canossa.99@gmail.com>
2022-05-09 14:17:24 +0200

    modified urlpatterns

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path, include
-from rest_framework import routers
-from comics import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+from django.contrib import admin
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api/v1/auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('admin/', admin.site.urls),
+    path('api/v1/auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/v1/', include('comics.urls', namespace='comics'))
 ]
