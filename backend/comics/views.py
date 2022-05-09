1ecceb6beaf21fd2b88d4900ed3a47e8729abfbe
Alessandro Canossa <alessandro.canossa.99@gmail.com>
2022-05-09 17:47:52 +0200

    minor refactoring

--- backend/comics/views.py
+++ backend/comics/views.py
@@ -2,8 +2,6 @@ from django.http import HttpRequest
 from rest_framework import views
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
-from rest_framework.decorators import api_view, permission_classes, renderer_classes
-from rest_framework.reverse import reverse
 
 from .models import User
 from .serializers import UserSerializer
@@ -13,7 +11,8 @@ class UserList(views.APIView):
 
     # permission_classes = [permissions.IsAdminUser]
 
-    def get(self, request: HttpRequest, format=None) -> Response:
+    @staticmethod
+    def get(request: HttpRequest, format=None) -> Response:
         queryset = User.objects.all()
         serializer = UserSerializer(
             queryset, many=True, context={'request': request})
@@ -23,7 +22,8 @@ class UserList(views.APIView):
 class UserDetails(views.APIView):
     # permission_classes = [permissions.IsAdminUser]
 
-    def get(self, request: HttpRequest, pk: int, format=None) -> Response:
+    @staticmethod
+    def get(request: HttpRequest, pk: int, format=None) -> Response:
         user = User.objects.get(pk=pk)
         serializer = UserSerializer(user, context={'request': request})
         return Response(serializer.data)
