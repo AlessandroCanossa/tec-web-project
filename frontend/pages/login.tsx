3c5fa6e65e4a4ee333958485eef49fc9a308f130
Alessandro Canossa <alessandro.canossa.99@gmail.com>
2022-05-24 02:34:41 +0200

    reverted to bootstrap but with formik

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,6 +1,17 @@
 import Head from "next/head";
 import Layout from "../components/layout";
-import FormInput from "../components/formInput";
+
+import { Formik } from "formik";
+import * as Yup from "yup";
+import styles from "../components/registration.module.css";
+
+import { Form, Button } from "react-bootstrap";
+
+const schema = Yup.object().shape({
+  email: Yup.string().email().required(),
+  password: Yup.string().required(),
+  remember_me: Yup.bool(),
+});
 
 const Login = () => {
   return (
@@ -9,40 +20,83 @@ const Login = () => {
         <title>Log In</title>
       </Head>
 
-      <div className="container flex flex-wrap mt-5 mx-auto p-5 bg-white max-w-xl ">
-        <div className=" w-full items-center mb-4">
-          <h2 className="text-2xl">Log into your account</h2>
-        </div>
-        <div className=" w-full mb-4 ">
-          <form action="/" method="GET">
-            <div>
-              <FormInput
-                inputType={"text"}
-                inputName={"username"}
-                inputLabel={"Username"}
-                errorMsg={""}
-              />
-              <FormInput
-                inputType={"password"}
-                inputName={"password"}
-                inputLabel={"Password"}
-                errorMsg={""}
-                // errorMsg={passwordError}
-                // changeHandler={handleChangePassword}
-              />
-            </div>
-            <label>
-              <input type="checkbox" name="rememberMe" className="mr-2"/>
-              Remember Me
-            </label>
-            <div className="text-center">
-              <button type={"submit"} className="btn btn_dark">
-                Log in
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+      <section className={`${styles.form_signin} w-100 m-auto`}>
+        <Formik
+          validationSchema={schema}
+          initialValues={{
+            email: "",
+            password: "",
+            remember_me: false,
+          }}
+          onSubmit={(values, actions) => {
+            console.log({ values, actions });
+            alert(JSON.stringify(values, null, 2));
+            actions.setSubmitting(false);
+          }}
+        >
+          {({
+            handleSubmit,
+            handleChange,
+            values,
+            errors,
+          }) => (
+            <Form noValidate onSubmit={handleSubmit}>
+              <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+              <Form.Group
+                className="mb-3 position-relative"
+                controlId="validationFormik01"
+              >
+                <Form.FloatingLabel label="Email address" className="mb-3">
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    placeholder="name@example.com"
+                    isInvalid={!!errors.email}
+                    // isValid={touched.email && !errors.email}
+                    value={values.email}
+                    onChange={handleChange}
+                  />
+                  <Form.Control.Feedback type="invalid" tooltip>
+                    {errors.email}
+                  </Form.Control.Feedback>
+                </Form.FloatingLabel>
+              </Form.Group>
+              <Form.Group
+                className="mb-3 position-relative"
+                controlId="validationFormik02"
+              >
+                <Form.FloatingLabel label="Password">
+                  <Form.Control
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    isInvalid={!!errors.password}
+                    // isValid={touched.password && !errors.password}
+                    value={values.password}
+                    onChange={handleChange}
+                  />
+                  <Form.Control.Feedback type="invalid" tooltip>
+                    {errors.password}
+                  </Form.Control.Feedback>
+                </Form.FloatingLabel>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formCheckbox">
+                <Form.Check
+                  type="checkbox"
+                  label="Remember me"
+                  name="remember_me"
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              <Button className="w-100" size="lg" variant="dark" type="submit">
+                Sign in
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </section>
     </Layout>
   );
 };
