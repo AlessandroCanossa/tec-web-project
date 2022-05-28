51859a30a2bc4148dbad00acc0395a65e9815c89
AlessandroCanossa <alessandro.canossa.99@gmail.com>
2022-05-28 21:39:38 +0200

    modified urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,6 +5,6 @@ from django.conf import settings
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('api/v1/auth/', include('rest_framework.urls', namespace='rest_framework')),
-                  path('api/v1/', include('comics.urls', namespace='comics'))
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('', include('comics.urls')),
+
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
